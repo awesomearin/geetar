8ac0cdd7d32fa113849a774dc0d30c6a3de65167
arinchandra <arinchandra5@gmail.com>
2022-10-18 13:12:48 -0700

    fart

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,9 @@
 package com.company;
 
-import java.sql.SQLOutput;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("mogo ");
+        System.out.println("mogo");
     }
 }
